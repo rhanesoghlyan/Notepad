da7bf5495b970ee1ed1f7096664654d705e6e490
rhanesoghlyan <robert.hanesoghlyan@makeinarmenia.com>
2019-08-19 12:17:24 +0400

    created interceptor to set authorization token in each request header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
+import {JwtInterceptor} from '@core/interceptor/jwt.interceptor';
 import {AppComponent} from './components/root/app.component';
 import {AppRoutingModule} from './app-routing.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+  ],
   bootstrap: [
     AppComponent
   ]
